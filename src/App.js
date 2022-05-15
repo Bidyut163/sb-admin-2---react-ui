6be898de250ee4732e94bf0f2fbecc440a48726e
Bidyut Deka <bidyutranjan464@gmail.com>
2022-05-15 23:37:39 +0530

    added home redirect component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Home from './components/frontend/Home';
 import Login from './components/frontend/Login';
 import Register from './components/frontend/Register';
 import ForgotPassword from './components/frontend/ForgotPassword';
@@ -11,6 +12,7 @@ const App = () => {
         <div className="App">
             <Router>
                 <Switch>
+                    <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={Login} />
                     <Route exact path="/register" component={Register} />
                     <Route
